637bfae480229c7514e3541dee885660778e2cd0
DawNip <nipunaperer@hotmail.com>
2016-08-10 12:11:22 +0530

    remove titles
    titles

--- home.php
+++ home.php
@@ -51,15 +51,15 @@
 <nav>
 <div>
   <ul class="v-menu block-shadow-impact min-size-required">
-      <li class="menu-title">First Title</li>
+
       <li><a href="#"><span class="mif-home icon"></span> Home</a></li>
-      <li class="divider"></li>
-      <li class="menu-title">Second Title</li>
+
+
       <li><a href="#"><span class="mif-user icon"></span> Profile</a></li>
       <li><a href="#"><span class="mif-calendar icon"></span> Calendar</a></li>
       <li><a href="#"><span class="mif-image icon"></span> Photo</a></li>
-      <li class="divider"></li>
-      <li class="menu-title">Third Title</li>
+  
+
       <li>
           <a href="#" class="dropdown-toggle"><span class="mif-my-location icon"></span> Location</a>
           <ul class="d-menu" data-role="dropdown">
